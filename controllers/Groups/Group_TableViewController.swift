6d7529fff9c7e794b6b325c6dfefb2225039f83d
newzelend <89086288+newzelend@users.noreply.github.com>
2021-09-05 12:15:17 +0300

    7

--- controllers/Groups/Group_TableViewController.swift
+++ controllers/Groups/Group_TableViewController.swift
@@ -6,21 +6,40 @@
 //
 
 import UIKit
+import RealmSwift
 
 class Group_TableViewController: UITableViewController {
     var groupItems: [GroupItem] = []
+    let groupDB = GroupDB()
+    var token: NotificationToken?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let localGroupsResults = groupDB.get()
+        
+        token = localGroupsResults.observe { (changes: RealmCollectionChange) in
+            
+            switch changes {
+            
+            case .initial(let results):
+                self.groupItems = Array(results)
+                self.tableView.reloadData()
+                
+            case .update(let results, _, _, _):
+                self.groupItems = Array(results)
+                self.tableView.reloadData()
+                
+            case .error(let error):
+                print("Error: ", error)
+            }
+        }
+        
         GroupAPI(Session.instance).get{ [weak self] groups in
             guard let self = self else { return }
-            self.groupItems = groups!.response.items
-            self.tableView.reloadData()
+                self.groupDB.addUpdate(groups!.response.items)
         }
     }
-    
-    // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return groupItems.count
